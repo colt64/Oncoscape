fe0f243d95f22dcb741616574b5d36981f8828bb
LisaMc <lmcferri@fredhutch.org>
2016-06-08 18:18:18 -0700

    first draft code for scaling chromosomes and pts

---
+++ r_modules/dataPackages/scripts/Networks_Chromosome_Patient_Scaling.R
@@ -0,0 +1,138 @@
+library(org.Hs.eg.db)
+library(jsonlite)
+
+printf = function (...) print (noquote (sprintf (...)))
+options(stringsAsFactors=FALSE)
+
+#--------------------------------------------------------------#
+
+mol_dir<- "../molecular_data/"
+hg19_dir <- "../molecular_data/hg19"
+ucsc_dir <- "../molecular_data/UCSC"
+
+chromosome_file <- "chromosome_lengths_hg19.json"
+centromere_file <- "centromere_position_hg19.json"
+genepos_file <- "gene_symbol_min_abs_start_hg19.json"
+geneset_file <- "genesets_by_symbol.json"
+
+mol_metadata_file <- "molecular_collections_metadata.txt"
+
+#----------------------------------------------------------------------------------------------------
+get.json <- function(directory, file)
+{
+	if(!grepl("/$", directory)) directory <- paste(directory, "/", sep="")
+
+	data.json <- fromJSON(paste(directory, file, sep=""))
+	return(data.json)
+}
+
+#----------------------------------------------------------------------------------------------------
+save.json <- function(dataObj, directory, file)
+{
+  if(!dir.exists(directory))
+    dir.create(file.path(directory), recursive=TRUE)
+  
+  if(!grepl("/$", directory)) directory <- paste(directory, "/", sep="")
+ 
+  outFile = paste(directory, file, sep="")
+  write(toJSON(dataObj, pretty=TRUE, digits=I(8)), file=paste(outFile,".json", sep = "") )
+  
+} # saveGraph
+
+#--------------------------------------------------------------#
+getChromosomeOffsets <- function(chromosomes, chrLengths, pLength){
+	
+	yCent <- max(pLength)
+	yOffset <- sapply(chromosomes, function(chr) {  yCent - pLength[chr] })
+	names(yOffset) <- chromosomes
+	yChrLengths <- sapply(chromosomes, function(chr) { yOffset[chr] + chrLengths[chr] })
+	names(yChrLengths) <- chromosomes
+	
+	yHeight <- max(unlist(yChrLengths))
+	xWidth <- yHeight * (4/3)  # 3x4 (y,x) ratio
+	numChrs <- length(chromosomes)
+	chrWidth <- xWidth/numChrs
+	xChrOffset <- sapply(1:numChrs, function(i) { chrWidth*i })
+	
+	chrCoordinates <- data.frame(name=chromosomes,length=unlist(chrLengths[chromosomes]),centromere=unlist(pLength), yOffset=unlist(yOffset), xOffset = unlist(xChrOffset))
+	rownames(chrCoordinates) <- chromosomes
+	
+	return(list(chrCoordinates =chrCoordinates, dim=c(xWidth, yHeight)))
+}
+#--------------------------------------------------------------#
+getChromosomePositions <- function(chromosomes, chrCoordinates){
+
+	chrPos <- lapply(chromosomes, function(chr){
+		offset = chrCoordinates[chr,"yOffset"]
+		data.frame(x=chrCoordinates[chr, "xOffset"], p=offset, c=offset+chrCoordinates[chr,"centromere"], q=offset+chrCoordinates[chr,"length"])
+	})
+	names(chrPos) <- chromosomes
+	return(chrPos)
+
+}
+#--------------------------------------------------------------#
+scaleSamplesToChromosomes <- function(mtx.xy, chrDim){
+	
+	xOffset <- -1* min(mtx.xy[,"x"])
+	yOffset <- -1* min(mtx.xy[,"y"])
+	
+	mtx.xy[,"x"] = mtx.xy[,"x"] + xOffset; mtx.xy[,"y"] = mtx.xy[,"y"] + yOffset
+		# offset mtx so min val is 0,0
+	
+	mtxDim(max(mtx.xy[,"x"]), max(mtx.xy[,"y"]))
+	
+	r2Chr <- sum(chrDim*chrDim)
+	r2Mtx <- sum(mtxDim*mtxDim)	
+	scale <- r2Chr/r2Mtx
+		# make diagonal of drawing regions equal
+		
+	mtx.xy <- mtx.xy * scale
+
+	return(mtx.xy)
+}
+#--------------------------------------------------------------#
+scaleGenesToChromosomes <- function(genePos, chrCoordinates){
+	
+	genePos_xy <- lapply(genePos, function(gene){
+		x <- chrCoordinates[gene[1], "xOffset"]
+		y <- chrCoordinates[gene[1], "yOffset"] + as.numeric(gene[2])
+		c(x,y)
+	})
+
+	return(genePos_xy)	
+	
+}
+
+#--------------------------------------------------------------#
+run.batch <- function(){
+
+	chrLengths <- get.json(hg19_dir,chromosome_file)
+	pLength <- get.json(hg19_dir,centromere_file)
+	genePos <- get.json(hg19_dir, genepos_file)
+	chromosomes <- c(seq(1:22), "X", "Y")
+
+	chrSpecs <- getChromosomeOffsets(chromosomes, chrLengths, pLength)
+	chrPos <- getChromosomePositions(chromosomes, chrSpecs$chrCoordinates)
+	save.json(chrPos, hg19_dir, "chromosome_coordinates_scaled")
+	
+	genePos_scaled <- scaleGenesToChromosomes(genePos, chrSpecs$chrCoordinates)
+	save.json(genePos_scaled, hg19_dir, paste(genepos_file, "scaled", sep="_"))
+
+	for(mdsFile in mdsFiles){
+		mtx <- fromJSON(mdsFile)
+		mtx_scaled <- scaleSamplesToChromosomes(mtx$data, chrSpecs$dim)
+		file= paste(mdsFile, "scaled", sep="_")
+		save.json(mtx_scaled, ucsc_dir, file)
+	}
+	
+	for (genesetName in names(genesets)){	
+		genes <- genesets[[genesetName]]
+		genesetPos <- genePos_scaled[genes]
+		file= paste("network_chrPos_", genesetName, sep="")
+		save.json(genesetPos, hg19_dir, file)
+	}	
+}
+
+#--------------------------------------------------------------#
+
+	run.batch()

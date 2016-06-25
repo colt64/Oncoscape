a78dca4582d52cff9aaf9d35fa8dbff8619fd22e
LisaMc <lmcferri@fredhutch.org>
2016-06-24 19:19:41 -0700

    working for mds calculation, drafted for scaling

--- dataPackages/scripts/networks.scale.mds.genomic.coordinates.R
+++ dataPackages/scripts/networks.scale.mds.genomic.coordinates.R
@@ -4,47 +4,83 @@ library(jsonlite)
 printf = function (...) print (noquote (sprintf (...)))
 options(stringsAsFactors=FALSE)
 
-commands >- c("mdsScaled", "geneScaled")
+commands <- c("mdsScaled", "geneScaled")
 args = commandArgs(trailingOnly=TRUE)
 if(length(args) != 0)
 	commands <- args
 
 #--------------------------------------------------------------#
 
-mol_dir<- "../molecular_data/"
-hg19_dir <- "../molecular_data/hg19"
-ucsc_dir <- "../molecular_data/UCSC"
-mds_orig_dir <- "../networks/mds/original"
-mds_scaled_dir <- "../networks/mds/scaled"
+mds_manifest <- "../manifests/os.mds.network.2016-06-23.manifest.json"
+hg19_manifest <- "../manifests/os.hg19.manifest.json"
 
-chromosome_file <- "chromosome_lengths_hg19.json"
-centromere_file <- "centromere_position_hg19.json"
-genepos_file <- "gene_symbol_min_abs_start_hg19.json"
-geneset_file <- "genesets_by_symbol.json"
+mds_scaled_dir <- "../data/networks/mds/scaled"
+hg19_dir <- "../data/molecular/hg19"
 
-mol_metadata_file <- "molecular_collections_metadata.txt"
-
-#----------------------------------------------------------------------------------------------------
-get.json <- function(directory, file)
-{
-	if(!grepl("/$", directory)) directory <- paste(directory, "/", sep="")
-
-	data.json <- fromJSON(paste(directory, file, sep=""))
-	return(data.json)
+#---------------------------------------------------------
+get.new.collection.index <- function(datasetName, dataTypeName){
+  
+  if(nrow(Manifest) == 0) return(1)
+  
+  dataObj <- subset(Manifest, dataset == datasetName && dataType == dataTypeName)
+  if(nrow(dataObj) == 0) return(1)
+  
+  return(nrow(dataObj$collections[[1]]) +1)
 }
-
+#---------------------------------------------------------
+add.new.collection <- function(datasetName, dataTypeName, collection){
+  
+  if(nrow(Manifest) == 0){	
+    newCollection <- data.frame(dataset=datasetName, dataType=dataTypeName)
+    newCollection$collections <- list(collection)
+    Manifest <<- newCollection
+    return()
+  }
+  
+  dataObj <- subset(Manifest, dataset == datasetName & dataType == dataTypeName)
+  if(nrow(dataObj) == 1){
+    Manifest[Manifest$dataset==datasetName & Manifest$dataType ==dataTypeName,"collections"] <<- list(rbind(dataObj$collections[[1]],collection))
+    return()
+  }
+  if(nrow(dataObj) == 0){	
+    newCollection <- data.frame(dataset=datasetName, dataType=dataTypeName)
+    newCollection$collections <- list(collection)
+    Manifest <<- rbind(Manifest, newCollection)
+    return()
+  }
+  stop(printf("add.new.collection found %d instances of dataset %s and dataType %s", length(dataObj), datasetName, dataTypeName))
+  
+}
 #----------------------------------------------------------------------------------------------------
-save.json <- function(dataObj, directory, file)
-{
+### Save Function Takes A matrix/data.frame + Base File Path (w/o extension) & Writes to Disk In Multiple (optionally specified) Formats
+os.data.save <- function(df, directory, file, format = c("tsv", "csv", "RData", "JSON")){
+
   if(!dir.exists(directory))
     dir.create(file.path(directory), recursive=TRUE)
   
   if(!grepl("/$", directory)) directory <- paste(directory, "/", sep="")
- 
   outFile = paste(directory, file, sep="")
-  write(toJSON(dataObj, pretty=TRUE, digits=I(8)), file=paste(outFile,".json", sep = "") )
+
   
-} # saveGraph
+  # Write Tab Delimited
+  if("tsv" %in% format)
+    write.table(df, file=paste(outFile,".tsv", sep = ""), quote=F, sep="\t")
+  
+  # Write CSV Delimited
+  if("csv" %in% format)
+    write.csv(df, file=paste(outFile,".csv",sep = ""), quote = F)
+  
+  # Write RData File
+  if("RData" %in% format)
+    save(df, file=paste(outFile,".RData", sep = "") )
+  
+  # Write JSON File
+  if("JSON" %in% format)
+  write(toJSON(df, pretty=TRUE, digits=I(8)), file=paste(outFile,".json", sep = "") )
+  
+  # Return DataFrame For Chaining
+  return(df)
+}
 
 #--------------------------------------------------------------#
 getChromosomeOffsets <- function(chromosomes, chrLengths, pLength, scaleFactor=1000){
@@ -116,10 +152,20 @@ scaleGenesToChromosomes <- function(genePos, chrCoordinates, scaleFactor=1000){
 #--------------------------------------------------------------#
 run.batch <- function(scaleFactor=10000){
 
-	chrLengths <- get.json(hg19_dir,chromosome_file)
-	pLength <- get.json(hg19_dir,centromere_file)
-	genePos <- get.json(hg19_dir, genepos_file)
-	genesets <- get.json(hg19_dir, geneset_file)
+	hg19_data <- fromJSON(hg19_manifest)
+
+	hg19 <- subset(hg19_data, dataset=="hg19")
+  
+	chromObj <- hg19[hg19$dataType=="chromosome", "collections"][[1]]
+	geneObj  <- hg19[hg19$dataType=="genes",      "collections"][[1]]
+	centObj  <- hg19[hg19$dataType=="centromere", "collections"][[1]]
+	genesets <- hg19[hg19$dataType=="genesets", "collections"][[1]]
+	
+	chrLengths <- fromJSON(paste(chromObj$directory, chromObj$file, ".json", sep=""))
+	pLength    <- fromJSON(paste(centObj$directory,  centObj$file,  ".json", sep=""))
+	genePos    <- fromJSON(paste(geneObj$directory,  geneObj$file,  ".json", sep=""))
+	genesets   <- fromJSON(paste(genesets$directory, genesets$file, ".json", sep=""))
+	
 	chromosomes <- c(seq(1:22), "X", "Y")
 
 	chrSpecs <- getChromosomeOffsets(chromosomes, chrLengths, pLength, scaleFactor=scaleFactor)
@@ -129,26 +175,65 @@ run.batch <- function(scaleFactor=10000){
 	genePos_scaled <- scaleGenesToChromosomes(genePos, chrSpecs$chrCoordinates, scaleFactor=scaleFactor)
 	save.json(genePos_scaled, hg19_dir, paste(genepos_file, "scaled", sep="_"))
 
-	mds_Files<- list.files(mds_orig_dir)
-
 	if("mdsScaled" %in% commands) {
+
+		mds_Files<- fromJSON(mds_manifest)
+		mds_Files <- subset(mds_Files, dataType=="mds")
+
 		for(mdsFile in mds_Files){
-			mtx <- get.json(mds_orig_dir, mdsFile)
-			mtx <- t(as.data.frame(mtx)); 
-			colnames(mtx) <- c("x", "y")
-			mtx_scaled <- scaleSamplesToChromosomes(mtx, chrSpecs$dim)
-			file= paste(gsub(".json", "", mdsFile), "scaled", sep="_")
-			save.json(mtx_scaled, mds_scaled_dir, file)
+			datasetName <- mdsFile$dataset
+			dataType <- mdsFile$dataType
+			collections <- mdsFile$collections[[1]]
+			
+			for(i in 1:nrow(collections)){
+				dataObj <- collections[i,]
+				process= c(dataObj$process, "scale10k")
+				processName <- paste(process, collapse="-")
+
+				mtx <- fromJSON(paste(dataObj$directory, dataObj$file,".json", sep=""))
+				mtx <- t(as.data.frame(mtx)); 
+				colnames(mtx) <- c("x", "y")
+				mtx_scaled <- scaleSamplesToChromosomes(mtx, chrSpecs$dim)
+				
+				index <- get.new.collection.index(datasetName, dataType)
+				file= paste(datasetName, dataType, index, processName, sep="_")
+				parent <- list(c(datasetName, dataType, dataObj$id))
+				collection <- data.frame(id=index, date=date,directory=mds_scaled_dir, file=file)
+				collection$parent <- list(parent)
+				collection$process <- list(process)
+
+				add.new.collection(datasetName, dataType, collection)
+				os.data.save(mtx_scaled, mds_scaled_dir, file, format="JSON")
+			}
 		}
 	}
 	if("geneScaled" %in% commands) {
+
+		datasetName = "genesets"
+		datatype = "position"
+
+		genePos_Files<- fromJSON(mds_manifest)
+		mds_Files <- subset(mds_Files, dataType=="mds")
+
 
 		for (genesetName in names(genesets)){	
 			genes <- genesets[[genesetName]]
 			map_genes <- intersect(genes, names(genePos_scaled))
 			genesetPos <- genePos_scaled[map_genes]
+			process <- c(genesetName,"scale10k")
+			processName <- paste(process, collapse="-")
+			
+			index <- get.new.collection.index(datasetName, dataType)
+			file= paste(datasetName, dataType, index, processName, sep="_")
+			parent <- list(c(datasetName, dataType, dataObj$id))
+			collection <- data.frame(id=index, date=date,directory=mds_scaled_dir, file=file)
+			collection$parent <- list(parent)
+			collection$process <- list(process)
+
+			add.new.collection(datasetName, dataType, collection)
+			
 			file= paste("network_chrPos_", genesetName, sep="")
-			save.json(genesetPos, hg19_dir, file)
+			os.data.save(genesetPos, hg19_dir, file, format="JSON")
 		}	
 	}
 }

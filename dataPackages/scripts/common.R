1802cee008c4a802f44acef78f5170af4f527e18
LisaMc <lmcferri@fredhutch.org>
2016-08-05 10:36:50 -0700

    linking in datapackages

---
+++ dataPackages/scripts/common.R
@@ -0,0 +1,315 @@
+# Library Imports ---------------------------------------------------------
+library(RUnit)
+library(R.utils)
+library(stringr)
+library(plyr)
+library(jsonlite)
+library(rmongodb)
+
+
+os.dataset.enumerations     <- fromJSON("../manifests/os.dataset.enumerations.json" )
+date <- as.character(Sys.Date())
+chromosomes <- c(seq(1:22), "X", "Y")
+
+dataset_map <- list(
+  brca=list(name="Breast", img= "DSbreast.png", beta=FALSE, source="TCGA"),
+  brain=list(name="Brain", img= "DSbrain.png", beta=FALSE, source="TCGA"),
+  gbm=list(name="Glioblastoma", img= "DSbrain.png", beta=TRUE, source="TCGA")
+)
+
+#---------------------------------------------------------
+connect.to.mongo <- function(host= "127.0.0.1", name = "", username = "", password = "", db = "admin"){
+	mongo <- mongo.create(host = host, name = name, username = username,
+  							password = password, db = db, timeout = 0L)
+	
+	stopifnot(mongo.is.connected(mongo))
+	return(mongo)
+}
+
+#---------------------------------------------------------
+close.mongo <- function(mongo){
+
+	if(mongo.is.connected(mongo) == TRUE) {
+	  # close connection
+	  mongo.destroy(mongo)
+	}
+}
+
+#---------------------------------------------------------
+mongo.collection.as.matrix <- function(collection, format=""){
+
+  cursor <- mongo.find(mongo, paste("oncoscape",collection, sep="."), query=list(), fields=list())
+  count <- mong.count(mongo, paste("oncoscape",collection, sep="."))
+  result_lst <- vector('list', count)
+  i <- 1
+  while (mongo.cursor.next(cursor)) {
+    result_lst[[i]] <- mongo.bson.to.list(mongo.cursor.value(cursor))
+    val <-geneRow$patients; 
+    null.val <- which(unlist(lapply(val, is.null)))
+    if(length(null.val)>0) val[null.val] <- NA
+    val <- unlist(val);
+    if(format == "as.numeric") val <- as.numeric(val)
+    
+    i <- i + 1
+  }
+  result_dt <- data.table::rbindlist(result_lst)
+
+  colnames(mtx) <- sapply(data.list, function(geneRow){ geneRow$gene})
+  rownames(mtx) <- names(data.list[[1]]$patients)
+  return(mtx)  
+}
+
+#---------------------------------------------------------
+convert.to.mtx <- function(data.list, format=""){
+  mtx <- sapply(data.list, function(geneRow){ 
+    val <-geneRow$patients; 
+    null.val <- which(unlist(lapply(val, is.null)))
+    if(length(null.val)>0) val[null.val] <- NA
+    val <- unlist(val);
+    if(format == "as.numeric") val <- as.numeric(val)
+#    if(all(is.null(val))){ val <- rep(NA, length(geneRow$patients))} 
+    val})
+  colnames(mtx) <- sapply(data.list, function(geneRow){ geneRow$gene})
+  rownames(mtx) <- names(data.list[[1]]$patients)
+  return(mtx)  
+}
+
+#---------------------------------------------------------
+mapProcess <- function(process){
+  
+	processFound <-	sapply(os.dataset.enumerations$dataType, function(typeMap){ process %in% unlist(typeMap) })
+	numMatches <- length(which(processFound))
+	if(numMatches==1)
+		return (names(os.dataset.enumerations$dataType)[which(processFound)])
+
+	stop(printf("mapProcess found %d matches for process %s", numMatches, process))
+	return(NA)
+}
+#---------------------------------------------------------
+### For any mutation file, create and save an indicator mut01 file
+save.mut01.from.mut <- function(mongo, result, dataset, dataType,source, parentID){
+  
+  mut.list <- result
+  
+  data.list <- lapply(result, function(geneSet){
+    patients <- lapply(geneSet$patients, function(pt){mut <- nchar(pt); mut01 <- ifelse(mut > 0, 1, 0 ); mut01 })
+    list(gene=geneSet$gene,min=min(unlist(patients)), max=max(unlist(patients)), patients = patients)
+  })    
+  
+  parent <- parentID
+  
+  save.collection(mongo, dataset=dataset, dataType="mut01",source=source, result=data.list,
+                              parent=parent, process=process,processName=process)
+
+}
+
+#---------------------------------------------------------
+collection.exists <- function(mongo, dataset, dataType,source,processName){
+
+  source <- unique(source)
+  if(length(source)>1) source <- list(source)
+  sourceName <- paste(unlist(source), collapse="-")
+
+  collection.uniqueName <- paste(dataset, dataType, sourceName, processName, sep="_")
+  collection.ns <- paste("oncoscape", collection.uniqueName, sep=".")
+  if(mongo.count(mongo, collection.ns) != 0){
+    print(paste(collection.uniqueName, " already exists.", sep=""))
+    return(TRUE)
+  }  
+  return(FALSE)
+
+}
+#---------------------------------------------------------
+save.collection <- function(mongo, dataset, dataType,source,result, parent, 
+                            process,processName){
+  
+  cat("-save collection\n")
+  
+  source <- unique(source)
+  if(length(source)>1) source <- list(source)
+  sourceName <- paste(unlist(source), collapse="-")
+  
+  collection.uniqueName <- paste(dataset, dataType, sourceName, processName, sep="_")
+  collection.uniqueName <- gsub("\\s+", "", tolower(collection.uniqueName))
+  collection.ns <- paste("oncoscape", collection.uniqueName, sep=".")
+  if(mongo.count(mongo, collection.ns) != 0){
+    print(paste(collection.uniqueName, " already exists. Skipping.", sep=""))
+    return()
+  }  
+  
+  newCollection <- list(dataset=dataset, dataType=dataType, date=date) 
+  newCollection$collection <- collection.uniqueName
+  newCollection$source <- source
+  newCollection$process <- process
+  newCollection$parent <- parent
+  
+  ## add to manifest file
+  mongo.insert(mongo, "oncoscape.manifest", newCollection)
+  
+  pass <- lapply(result, function(el){mongo.insert(mongo, collection.ns, as.list(el))})
+  if(!all(unlist(pass))){
+    print("ERROR: result not inserted into mongodb.")
+    return()
+  }
+  
+  newID <-  mongo.find.one(mongo, "oncoscape.manifest", 
+                           query=newCollection, fields=list("_id"))
+  
+  ## add to lookup table  
+  lookup.ns <-  "oncoscape.lookup_oncoscape_datasources"
+  query <- list("disease"=dataset)
+  datasource <- mongo.find.one(mongo, lookup.ns, query)
+  
+  if(length(datasource)==0){
+    data.list <- list(disease = dataset, source = dataset_map[[dataset]]$source,beta = dataset_map[[dataset]]$beta)
+    data.list$name = dataset_map[[dataset]]$name
+    data.list$img = dataset_map[[dataset]]$img
+  }else{
+    data.list <- mongo.bson.to.list(datasource)
+  }
+  
+  if(dataType %in% c("cnv","mut01", "mut", "rna", "protein", "methylation")){
+    # update molecular
+    
+    add.collection <- list(data.frame(source=source, type=dataType, collection=collection.uniqueName))
+    if("molecular" %in% names(data.list)){
+      data.list$molecular <- c(data.list$molecular, add.collection)
+    }else{data.list$molecular <- add.collection}
+    
+  }else if(dataType %in% c("mds", "pcaScores")){
+    #update calculated
+    add.collection <- list(data.frame(source=source, type=dataType, collection=collection.uniqueName))
+    if("calculated" %in% names(data.list)){
+      data.list$calculated	<-c(data.list$calculated, add.collection)
+    } else {data.list$calculated <- add.collection }
+    
+  }else if(dataType %in% c("edges")){
+    #update edges
+    add.collection <- list(data.frame(name=process$geneset,edges=collection.uniqueName, 
+                           patientWeights=paste(dataset, "ptDegree", source, processName, sep="_"), 
+                           genesWeights=paste(dataset, "geneDegree", source, processName, sep="_")))
+    if("edges" %in% names(data.list)){
+      data.list$edges	<- c(data.list$edges, add.collection)
+    } else {data.list$edges <- add.collection }
+    
+  }else if(dataType %in% c("ptDegree", "geneDegree")){
+    print(paste(dataType, "lookup info processed with edge creation", sep=" "))
+    return()
+  }else if(dataType %in% c("patient", "drug", "radiation", "followUp-v1p0","followUp-v1p5", "followUp-v2p1", "followUp-v4p0", "newTumor", "newTumor-followUp-v4p0", "otherMalignancy-v4p0")){
+    #update patient
+    add.collection <- list()
+    add.collection[dataType] <- collection.uniqueName
+    if("collections" %in% names(data.list)){
+      data.list$collections	<- c(data.list$collections, add.collection)
+    } else {data.list$collections <- add.collection }
+    
+  }else if(dataType %in% c("chromosome", "centromere", "genes")){
+    #update patient
+    add.collection <- list()
+    add.collection[dataType] <- collection.uniqueName
+    if("location" %in% names(data.list)){
+      data.list$location	<- c(data.list$location, add.collection)
+    } else {data.list$location <- add.collection }
+    
+  }else if(dataType %in% c("genesets")){
+    add.collection <- list(data.frame(source=source, type=dataType, collection=collection.uniqueName))
+    if("category" %in% names(data.list)){
+      data.list$category <- c(data.list$category, add.collection)
+    }else{data.list$category <- add.collection}
+    
+  }else{
+    print(paste("WARNING: data type not recognized:", dataType, sep=" "))
+    return()
+  }
+  
+  ## insert lookup into mongo collection
+  mongo.update(mongo, lookup.ns, query, data.list, mongo.update.upsert)
+  
+  if(dataType == "mut")
+    save.mut01.from.mut(mongo, result, dataset, dataType,source, parentID=newID)
+  
+}
+
+#---------------------------------------------------------
+# Aggregate unmapped column names and classes into a single list  
+appendList <- function (x, val) 
+{
+    if(!is.list(x) && !is.list(val)) return(x)
+    xnames <- names(x)
+    for (v in names(val)) {
+        x[[v]] <- if (v %in% xnames && is.list(x[[v]]) && is.list(val[[v]])) 
+            appendList(x[[v]], val[[v]])
+        else unique(c(x[[v]], val[[v]]))
+    }
+    x
+}
+
+
+#--------------------------------------------------------------#
+get.chromosome.dimensions <- function(scaleFactor=100000){
+  
+  chrPosScaledObj <- mongo.find.all(mongo, "oncoscape.manifest", list(dataset="hg19",dataType="chromosome", process=list(scale=scaleFactor)))[[1]]
+  
+  chrCoord <- mongo.find.all(mongo, paste("oncoscape",chrPosScaledObj$collection, sep="."))[[1]][["data"]]
+  chrPos_xy <-t(sapply(chromosomes, function(chr){ return(c(chrCoord[[chr]]$x, chrCoord[[chr]]$q))}))
+  chrDim <- c(max(chrPos_xy[,1]), max(chrPos_xy[,2]))
+  
+  return(chrDim)
+}
+#--------------------------------------------------------------#
+scaleSamplesToChromosomes <- function(mtx, chrDim, dim.names=c("x", "y", "z")){
+  
+  mtx <- apply(mtx, 2, function(col){ -1* min(col) + col})
+  # offset mtx so min val is 0,0
+  mtx.max <- apply(mtx, 2, max)
+  
+  r2Chr <- sum(chrDim*chrDim)
+  r2Mtx <- sum(mtx.max*mtx.max)	
+  scale <- sqrt(r2Chr/r2Mtx)
+  # make diagonal of drawing regions equal
+  
+  mtx <- mtx * scale
+  mtx <- round(mtx)
+  
+  list.coord <- lapply(rownames(mtx), function(name){
+    vals <- data.frame(t(mtx[name,dim.names]))
+  })
+  names(list.coord) <- rownames(mtx)
+  return(list.coord)
+}
+#--------------------------------------------------------------#
+scaleGenesToChromosomes <- function(genePos, chrCoordinates, scaleFactor=1000){
+  
+  genePos_xy <- lapply(genePos, function(gene){
+    x <- chrCoordinates[gene[1], "xOffset"]
+    y <- chrCoordinates[gene[1], "yOffset"] + as.numeric(gene[2])/scaleFactor
+    data.frame(x=round(x),y=round(y))
+  })
+  
+  return(genePos_xy)	
+  
+}
+
+#--------------------------------------------------------------#
+save.batch.genesets.scaled.pos <- function(scaleFactor=100000){
+  
+  geneObj<- mongo.find.all(mongo, "oncoscape.manifest", list(dataset="hg19", dataType="genes", process=list(scale=scaleFactor)))[[1]]
+  genePos_scaled <- mongo.find.all(mongo, paste("oncoscape",geneObj$collection, sep="."))[[1]]
+  
+  genesetObj <-  mongo.find.all(mongo, "oncoscape.manifest", list(dataset="hg19",dataType="genesets"))[[1]]
+  genesets <- mongo.find.all(mongo, paste("oncoscape",genesetObj$collection, sep="."))
+  
+  process <- list(calculation="scaled", scaleFactor=scaleFactor); 
+  processName <- paste(process, collapse="-")
+  parent <- list(geneObj$`_id`,genesetObj$`_id`)
+  
+  result <- lapply(genesets, function(geneSet){	
+    genes <- geneSet$genes
+    map_genes <- intersect(genes, names(genePos_scaled$data))
+    genesetPos <- genePos_scaled$data[map_genes]
+    list(type="geneset", name=geneSet$name, scale=scaleFactor, data=genesetPos)
+  }	)
+  
+  save.collection(mongo, dataset=geneObj$dataset, dataType="genesets",source=geneObj$source, result=result,
+                  parent=parent, process=process,processName=processName)
+}
---
+++ dataPackages/scripts/hg19.save.genomic.coordinates.R
@@ -0,0 +1,216 @@
+
+# Configuration -----------------------------------------------------------
+rm(list = ls(all = TRUE))
+options(stringsAsFactors = FALSE)
+
+source("common.R")
+library(org.Hs.eg.db)
+
+cytoband_url <- "http://hgdownload.cse.ucsc.edu/goldenPath/hg38/database/cytoBand.txt.gz"
+chromosomes <- c(seq(1:22), "X", "Y")
+date <- as.character(Sys.Date())
+scaleFactor <- 100000 
+
+#----------------------------------------------------------------------------------------------------
+getChromosomeLengths <- function(){
+	return( org.Hs.egCHRLENGTHS )
+}
+#----------------------------------------------------------------------------------------------------
+getEntrezSymbolMap <- function(){
+	symbolMap <- org.Hs.egSYMBOL
+	mapped_genes <- mappedkeys(symbolMap)
+	
+	return(as.list(symbolMap[mapped_genes]) )
+	
+}#----------------------------------------------------------------------------------------------------
+getGenePositions_Entrez <- function(){
+
+	genePos <- org.Hs.egCHRLOC
+	# Get the entrez gene identifiers that are mapped to chromosome locations
+
+	mapped_genes <- mappedkeys(genePos)
+	return( as.list(genePos[mapped_genes]) )
+}
+#----------------------------------------------------------------------------------------------------
+getGenePositions_Symbol <- function(){
+	EntrezGenePos <- getGenePositions_Entrez()
+	  # list of gene start positions, with entrez ID as name.  
+	  # Multiple positions reported as named integer vector (name=chromosome)
+	  # Locations measured as the number of base pairs from the p (5' end of the sense strand) to q (3' end of the sense strand) arms. 
+	  # Locations on antisense strand have a leading "-" sign (e. g. -1234567)
+	
+	EntrezSymbolMap <- getEntrezSymbolMap()
+	
+	noSymbol<- which(sapply(EntrezSymbolMap, function(gene){is.na(gene)}))
+	stopifnot(length(noSymbol) == 0)
+		# all EntrezGene IDs should have symbol
+	stopifnot(length(setdiff(names(EntrezGenePos), names(EntrezSymbolMap))) ==0)
+
+	names(EntrezGenePos) <- sapply(names(EntrezGenePos), function(entrezID) { EntrezSymbolMap[[entrezID]] })
+		# Set EntrezGenePos names to gene symbol using EntrezSymbolMap
+	
+	return (EntrezGenePos)
+}
+
+#----------------------------------------------------------------------------------------------------
+saveChromosome_Coordinates <- function(){
+	
+	chrLengths <- getChromosomeLengths()
+	df<-data.frame(t(chrLengths))
+	names(df) <- names(chrLengths)
+
+	result = list(dataset="hg19", type="chromosome", process="length", data=df)
+	
+	save.collection(mongo, dataset="hg19", dataType="chromosome", source="orgHs",
+	                result=list(result),parent=NA, process="length",processName="length")
+}	
+#----------------------------------------------------------------------------------------------------
+saveGene_Coordinates <- function(){
+
+	genePos <- getGenePositions_Symbol()
+		# list of all start locations for each gene symbol
+			
+	genePos_min <- lapply(genePos, function(geneLocations) { 
+		geneLocations <- geneLocations[names(geneLocations) %in% chromosomes]
+		if(length(geneLocations)==0) return("NULL")
+
+		minLoc <- min(abs(geneLocations))
+		chrName <- unique(names(geneLocations)[which(abs(geneLocations) == minLoc)])
+		c(chrName, as.integer(minLoc))
+	})
+	# list with chr name & min (absolute value) chr location for each gene symbol
+	
+	notMapped <- which(genePos_min == "NULL")
+	genePos_min[notMapped] <- NULL
+		# removes gene positions that map to chromosomes outside our list (ie 1-22, X, Y)
+
+	process = list("position", "min", "abs", "start")
+	processName = paste(unlist(process), collapse="-")
+	result = list(dataset="hg19", type="genes", process=process, data=genePos_min)
+	
+	save.collection(mongo, dataset="hg19", dataType="genes", source="orgHs",
+	                result=list(result),parent=NA, process=process,processName=processName)
+	
+}
+#----------------------------------------------------------------------------------------------------
+saveCentromere_Coordinates <- function(cytoband_url){
+	
+	temp <- tempfile()
+	download.file(cytoband_url,temp)
+	cytobands <- read.delim(gzfile(temp), header=F)
+	unlink(temp)
+
+	colnames(cytobands) <- c("chr", "start", "end", "cytoband", "other")
+	cytobands$chr <- sub("chr", "", cytobands$chr)
+	
+	centromere <- sapply(chromosomes, function(chrName){
+		chr_cyto <- subset(cytobands, chr == chrName)
+		chr_cyto_p <- subset(chr_cyto, grepl("^p", cytoband))
+		chr_p_end <- max(chr_cyto_p$end)
+			# == chr_q_start 
+		
+		chr_p_end
+	})
+	
+	df<-data.frame(t(centromere))
+	names(df) <- names(centromere)
+	
+	result = list(dataset="hg19", type="centromere", process="position", data=df)
+	
+	save.collection(mongo, dataset="hg19", dataType="centromere", source="orgHs",
+	                result=list(result),parent=NA, process="position",processName="position")
+	
+}
+
+#--------------------------------------------------------------#
+getChromosomeOffsets <- function(chromosomes, chrLengths, pLength, scaleFactor=1000){
+  
+  pLength = unlist(pLength)/scaleFactor
+  chrLengths = unlist(chrLengths)/scaleFactor
+  
+  yCent <- max(pLength)
+  yOffset <- sapply(chromosomes, function(chr) {  yCent - pLength[chr] })
+  names(yOffset) <- chromosomes
+  yChrLengths <- sapply(chromosomes, function(chr) { yOffset[chr] + chrLengths[chr] })
+  names(yChrLengths) <- chromosomes
+  
+  yHeight <- max(unlist(yChrLengths))
+  xWidth <- yHeight * (4/3)  # 3x4 (y,x) ratio
+  numChrs <- length(chromosomes)
+  chrWidth <- xWidth/numChrs
+  xChrOffset <- sapply(1:numChrs, function(i) { chrWidth*i })
+  
+  chrCoordinates <- data.frame(name=chromosomes,length=unlist(chrLengths[chromosomes]),centromere=unlist(pLength), yOffset=unlist(yOffset), xOffset = unlist(xChrOffset))
+  rownames(chrCoordinates) <- chromosomes
+  
+  return(list(chrCoordinates =chrCoordinates, dim=c(xWidth, yHeight)))
+}
+#--------------------------------------------------------------#
+getChromosomePositions <- function(chromosomes, chrCoordinates){
+  
+  chrPos <- lapply(chromosomes, function(chr){
+    offset = chrCoordinates[chr,"yOffset"]
+    data.frame(x=chrCoordinates[chr, "xOffset"], p=offset, c=offset+chrCoordinates[chr,"centromere"], q=offset+chrCoordinates[chr,"length"])
+  })
+  names(chrPos) <- chromosomes
+  return(chrPos)
+  
+}
+
+
+#--------------------------------------------------------------#
+#creates 2 files: scaled positions of chromosomes and all genes with offsets to align centromeres
+run.scale.chr.genes <- function(scaleFactor=10000){
+  
+  # define data objects
+  chrLenObj <- mongo.find.all(mongo, "oncoscape.manifest", list(dataset="hg19", dataType="chromosome", process="length"))[[1]]
+  genePosObj  <- mongo.find.all(mongo, "oncoscape.manifest", list(dataset="hg19", dataType="genes", process=c("position", "min", "abs", "start")))[[1]]
+  centPosObj  <- mongo.find.all(mongo, "oncoscape.manifest", list(dataset="hg19", dataType="centromere",process="position"))[[1]]
+  
+  chrLengths <- mongo.find.all(mongo, paste("oncoscape",chrLenObj$collection, sep="."), list())[[1]]
+  pLength    <- mongo.find.all(mongo, paste("oncoscape",centPosObj$collection, sep="."), list())[[1]]
+  genePos    <- mongo.find.all(mongo, paste("oncoscape",genePosObj$collection, sep="."), list())[[1]]
+  
+  chromosomes <- c(seq(1:22), "X", "Y")
+  
+  ## calculate Chromosome & Gene positions with scaling
+  chrSpecs <- getChromosomeOffsets(chromosomes, chrLengths$data, pLength$data, scaleFactor=scaleFactor)
+  chrPos <- getChromosomePositions(chromosomes, chrSpecs$chrCoordinates)
+  genePos_scaled <- scaleGenesToChromosomes(genePos$data, chrSpecs$chrCoordinates, scaleFactor=scaleFactor)
+  
+  ## create collections
+  process <- list(scale=scaleFactor); processName <- paste(process, collapse="-")
+  ## Chr Positions
+  parent <- list(chrLenObj$`_id`, centPosObj$`_id`)
+  result <- list(type="chromosome", scale=scaleFactor, data=chrPos)
+  save.collection(mongo, dataset=chrLenObj$dataset, dataType=chrLenObj$dataType,source=chrLenObj$source, result=list(result),
+                              parent=parent, process=list(process),processName=processName)
+  ## Gene Positions
+  parent <- list(genePosObj$`_id`, chrLenObj$`_id`)
+  result <- list(type="geneset", scale=scaleFactor, data=genePos_scaled)
+  save.collection(mongo, dataset=genePosObj$dataset, dataType=genePosObj$dataType,source=genePosObj$source, result=list(result),
+                              parent=parent, process=list(process),processName=processName)
+  
+}	
+
+
+
+
+
+#----------------------------------------------------------------------------------------------------
+## must first initialize server (through shell >mongod)
+
+#mongo <- connect.to.mongo(host="oncoscape-dev-db1.sttrcancer.io", username="oncoscape", password=password)
+mongo <- connect.to.mongo()
+
+	saveChromosome_Coordinates()
+	saveGene_Coordinates()
+	saveCentromere_Coordinates(cytoband_url)	
+	
+	run.scale.chr.genes(scaleFactor)
+	
+close.mongo(mongo)
+
+
+
+	
